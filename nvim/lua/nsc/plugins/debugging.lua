f12b0ce82477778513a3059005b337019295d4ad
nelson-nsc <nelson.ngai@huboo.com>
2024-04-16 22:07:13 +0100

    set debugging root directory

--- nvim/lua/nsc/plugins/debugging.lua
+++ nvim/lua/nsc/plugins/debugging.lua
@@ -11,6 +11,16 @@ return {
     local dap, dapui = require("dap"), require("dapui")
     require("dap-python").setup(path)
     dapui.setup()
+
+    -- Python Debugging configuration. Set cwd as root directory.
+    table.insert(require("dap").configurations.python, {
+      type = "python",
+      request = "launch",
+      name = "Run from Root",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+    })
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -23,8 +33,8 @@ return {
     dap.listeners.before.event_exited.dapui_config = function()
       dapui.toggle()
     end
-    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, {})
-    vim.keymap.set('n', '<Leader>dc', dap.continue, {})
-    vim.keymap.set('n', "<Leader>dw", dapui.toggle, {})
-  end
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<Leader>dw", dapui.toggle, {})
+  end,
 }
