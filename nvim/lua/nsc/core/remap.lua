4af805ae45675a187049a02335901ab7421b8fa6
nelson-nsc <nelson.ngai@huboo.com>
2024-04-27 00:32:26 +0100

    set key bindings

--- nvim/lua/nsc/core/remap.lua
+++ nvim/lua/nsc/core/remap.lua
@@ -3,10 +3,15 @@ local g = vim.g
 g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
 -- Buffers
-vim.keymap.set('n', '<leader>bn', vim.cmd.bnext)
-vim.keymap.set('n', '<leader>bp', vim.cmd.bprevious)
-vim.keymap.set('n', '<leader>bd', vim.cmd.bdelete)
-vim.keymap.set('n', '<leader>bvs', vim.cmd.vsplit)
-vim.keymap.set('n', '<leader>bhs', vim.cmd.split)
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+vim.keymap.set("n", "<leader>bvs", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>bhs", vim.cmd.split)
+
+local n_keymap = function(lhs, rhs)
+	vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
+end
+
+n_keymap("<Leader>w", ":bp|bd #")
--- nvim/lua/nsc/plugins/debugging.lua
+++ nvim/lua/nsc/plugins/debugging.lua
@@ -1,64 +1,70 @@
 return {
-  "mfussenegger/nvim-dap",
-  ft = "python",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "mfussenegger/nvim-dap-python",
-    "nvim-neotest/nvim-nio",
-  },
-  config = function()
-    local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-    local dap, dapui = require("dap"), require("dapui")
-    require("dap-python").setup(path)
-    dapui.setup()
+	"mfussenegger/nvim-dap",
+	ft = "python",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"mfussenegger/nvim-dap-python",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+		local dap, dapui = require("dap"), require("dapui")
+		require("dap-python").setup(path)
+		dapui.setup()
 
-    -- Python Debugging configuration. Set cwd as root directory.
-    table.insert(require("dap").configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Client Reports",
-      program = "${file}",
-      cwd = vim.fn.getcwd() .. "/projects/client-reports",
-    })
+		-- Python Debugging configuration. Set cwd as root directory.
+		table.insert(require("dap").configurations.python, {
+			type = "python",
+			request = "launch",
+			name = "Client Reports",
+			program = "${file}",
+			cwd = vim.fn.getcwd() .. "/projects/client-reports",
+		})
 
-    table.insert(require("dap").configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Perfect Workforce",
-      program = "${file}",
-      cwd = vim.fn.getcwd() .. "/projects/perfect_workforce",
-    })
+		table.insert(require("dap").configurations.python, {
+			type = "python",
+			request = "launch",
+			name = "Perfect Workforce",
+			program = "${file}",
+			cwd = vim.fn.getcwd() .. "/projects/perfect_workforce",
+		})
 
-    table.insert(require("dap").configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Huboo Utils",
-      program = "${file}",
-      cwd = vim.fn.getcwd() .. "/lib/huboo-utils",
-    })
+		table.insert(require("dap").configurations.python, {
+			type = "python",
+			request = "launch",
+			name = "Huboo Utils",
+			program = "${file}",
+			cwd = vim.fn.getcwd() .. "/lib/huboo-utils",
+		})
 
-    table.insert(require("dap").configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Root",
-      program = "${file}",
-      cwd = vim.fn.getcwd(),
-    })
+		table.insert(require("dap").configurations.python, {
+			type = "python",
+			request = "launch",
+			name = "Root",
+			program = "${file}",
+			cwd = vim.fn.getcwd(),
+		})
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.toggle()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.toggle()
-    end
-    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-    vim.keymap.set("n", "<Leader>dw", dapui.toggle, {})
-  end,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.toggle()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.toggle()
+		end
+		vim.keymap.set("n", "<Leader>dd", dap.run, {})
+		vim.keymap.set("n", "<Leader>dr", dap.restart, {})
+		vim.keymap.set("n", "<Leader>dt", dap.terminate, {})
+		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<Leader>di", dap.step_into, {})
+		vim.keymap.set("n", "<Leader>dn", dap.step_over, {})
+		vim.keymap.set("n", "<Leader>do", dap.step_out, {})
+		vim.keymap.set("n", "<Leader>dw", dapui.toggle, {})
+	end,
 }
