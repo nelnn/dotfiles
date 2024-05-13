fb70d3be293652bba5abf23e98bea69bc5b3ea60
nelnsc <nelsonngai.nsc@gmail.com>
2024-05-13 19:29:38 +0100

    add tab keymaps

--- nvim/lua/nsc/core/remap.lua
+++ nvim/lua/nsc/core/remap.lua
@@ -1,4 +1,7 @@
 local g = vim.g
+local n_keymap = function(lhs, rhs)
+  vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
+end
 
 g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -10,8 +13,17 @@ vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
 vim.keymap.set("n", "<leader>bvs", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>bhs", vim.cmd.split)
 
-local n_keymap = function(lhs, rhs)
-	vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
-end
+n_keymap("<Leader>w", ":bp|bd #") -- Close current buffer
 
-n_keymap("<Leader>w", ":bp|bd #")
+-- Tabs
+n_keymap("<leader>t", ":tabnew")
+n_keymap("<leader>tt", ":tabc")
+n_keymap("<leader>1", "1gt")
+n_keymap("<leader>2", "2gt")
+n_keymap("<leader>3", "3gt")
+n_keymap("<leader>4", "4gt")
+n_keymap("<leader>5", "5gt")
+n_keymap("<leader>6", "6gt")
+n_keymap("<leader>7", "7gt")
+n_keymap("<leader>8", "8gt")
+n_keymap("<leader>9", "9gt")
