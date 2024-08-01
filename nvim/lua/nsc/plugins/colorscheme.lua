21f83206cf3f8a30e8acd1ec1ff82d6a7042987c
nelnsc <nelsonngai.nsc@gmail.com>
2024-08-01 20:57:15 +0100

    add colorscheme

--- nvim/lua/nsc/plugins/colorscheme.lua
+++ nvim/lua/nsc/plugins/colorscheme.lua
@@ -6,10 +6,27 @@
 --   end,
 -- }
 
+-- return {
+--   "catppuccin/nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd([[colorscheme catppuccin-macchiato]])
+--   end,
+-- }
+
 return {
-	"catppuccin/nvim",
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme catppuccin-macchiato]])
-	end,
+  "EdenEast/nightfox.nvim",
+  priority = 1000,
+  config = function()
+    require('nightfox').setup({
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        }
+      }
+    })
+    vim.cmd([[colorscheme carbonfox]])
+  end,
 }
