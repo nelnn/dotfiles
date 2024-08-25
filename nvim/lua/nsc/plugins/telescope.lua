134be28a20f80555a7d56db130afa2e6e5db43e2
nelnsc <nelsonngai.nsc@gmail.com>
2024-08-25 20:13:58 +0100

    update telescope rip grep

--- nvim/lua/nsc/plugins/telescope.lua
+++ nvim/lua/nsc/plugins/telescope.lua
@@ -5,17 +5,20 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope-live-grep-args.nvim",
     },
     config = function()
       require("nvim-treesitter").setup()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>fd', builtin.git_commits, {})
       vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
       vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+      vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 
 
       local telescope = require("telescope")
@@ -24,6 +27,8 @@ return {
       -- Clone the default Telescope configuration
       local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
+      local actions = require "telescope.actions"
+      local lga_actions = require("telescope-live-grep-args.actions")
       -- I want to search in hidden/dot files.
       table.insert(vimgrep_arguments, "--hidden")
       -- I don't want to search in the `.git` directory.
@@ -40,21 +45,31 @@ return {
             -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
             find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
           },
-        },
-      })
-
-      local actions = require "telescope.actions"
-      require("telescope").setup {
-        pickers = {
           buffers = {
             mappings = {
               i = {
                 ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
               }
-            }
+            },
+          }
+        },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = {         -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                -- freeze the current list and start a fuzzy search in the frozen list
+                ["<C-space>"] = actions.to_fuzzy_refine,
+              },
+            },
           }
         }
-      }
+      })
+
+      telescope.load_extension("live_grep_args")
     end
   },
 }
