9e39f586ebdbd4fe7585c0fea7c5bd0a30563183
nelnsc <nelsonngai.nsc@gmail.com>
2024-08-25 22:27:47 +0100

    diable change detection

--- nvim/lua/nsc/lazy.lua
+++ nvim/lua/nsc/lazy.lua
@@ -11,4 +11,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("nsc.plugins")
+require("lazy").setup("nsc.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
