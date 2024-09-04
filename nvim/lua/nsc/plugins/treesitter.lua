b558767bfaa46cbdc615e0a58adabc93f68025f9
nelson-nsc <nelson.ngai@huboo.com>
2024-09-04 18:20:13 +0100

    add dockerfile

--- nvim/lua/nsc/plugins/treesitter.lua
+++ nvim/lua/nsc/plugins/treesitter.lua
@@ -1,40 +1,41 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "python",
-        "css",
-        "javascript",
-        "typescript",
-        "html",
-        "regex",
-        "toml",
-        "yaml",
-        "latex",
-        "markdown",
-        "markdown_inline",
-      },
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- A list of parser names, or "all" (the five listed parsers should always be installed)
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"python",
+				"css",
+				"javascript",
+				"typescript",
+				"html",
+				"regex",
+				"toml",
+				"yaml",
+				"latex",
+				"markdown",
+				"markdown_inline",
+				"dockerfile",
+			},
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
 
-      -- List of parsers to ignore installing (or "all")
-      -- ignore_install = { "javascript" },
+			-- List of parsers to ignore installing (or "all")
+			-- ignore_install = { "javascript" },
 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
