edf94c68aded817c0ceec3f696167d141749a674
nelnsc <nelsonngai.nsc@gmail.com>
2024-10-08 21:47:52 +0100

    update symlink script

--- config.sh
+++ config.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# TODO: This is created by Claude. May need an update.
-
 # Source and destination directories
 SOURCE_DIR="$HOME/.dotfiles/.config"
-DEST_DIR="$HOME/.config"
 
-# Create the destination directory if it doesn't exist
-mkdir -p "$DEST_DIR"
+# list of directories to symlink in ${homedir}
+directories=(alacritty karabiner lf nvim python tmux zk zsh)
 
-# Loop through each directory in the source
-for dir in "$SOURCE_DIR"/*; do
-    if [ -d "$dir" ]; then
-        # Get the base name of the directory
-        base_name=$(basename "$dir")
-        
-        # Full path of the destination link
-        dest_link="$DEST_DIR/$base_name"
-        
-        # Check if a link already exists
-        if [ -L "$dest_link" ]; then
-            echo "Updating existing link for $base_name"
-            ln -sf "$dir" "$dest_link"
-        elif [ -e "$dest_link" ]; then
-            echo "Warning: $dest_link already exists and is not a symlink. Skipping."
-        else
-            echo "Creating new link for $base_name"
-            ln -s "$dir" "$dest_link"
-        fi
-    fi
+# create symlinks (will overwrite old dotfiles)
+# for directory in "$SOURCE_DIR"/*; do  # use this to apply for all files/directories
+for directory in "${directories[@]}"; do
+    echo "Running ln -sf ${SOURCE_DIR}/${directory} ${HOME}/.config/"
+    ln -sf "${SOURCE_DIR}/${directory}" "${HOME}/.config"
 done
-
-echo "Linking complete!"
