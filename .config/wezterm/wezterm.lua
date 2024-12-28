706a59b1c98a857cb12bbcd65a02c82278f15a3c
nelnsc <nelsonngai.nsc@gmail.com>
2024-12-28 16:00:39 +0000

    remove unsafed apps

--- .config/wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
--- config.color_scheme = 'AdventureTime'
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-
-return config
