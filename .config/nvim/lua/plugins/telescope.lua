7a7e2364d0488541fc0d8b640d2bd8f273ad6ea7
nelnsc <nelsonngai.nsc@gmail.com>
2025-01-08 23:40:34 +0000

    add live grep args flag

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -50,7 +50,7 @@ return {
 
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set("n", "<leader>fG", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>-uuu")
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>fd', builtin.git_commits, {})
