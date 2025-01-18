544e330475b5fa1f8ddcb5c0c4a33fa8031ff49b
nelnsc <nelsonngai.nsc@gmail.com>
2025-01-18 21:36:30 +0000

    add mini-nvim

---
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -0,0 +1,29 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.ai").setup()
+    require("mini.surround").setup()
+    require('mini.comment').setup()
+    require('mini.pairs').setup()
+    require('mini.splitjoin').setup()
+    require('mini.jump').setup()
+    require('mini.trailspace').setup()
+    require('mini.cursorword').setup()
+    require('mini.move').setup({
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<leader>h",
+        right = "<leader>l",
+        down = "<leader>j",
+        up = "<leader>k",
+
+        -- Move current line in Normal mode
+        line_left = "<leader>h",
+        line_right = "<leader>l",
+        line_down = "<leader>j",
+        line_up = "<leader>k",
+      },
+    })
+  end
+}
