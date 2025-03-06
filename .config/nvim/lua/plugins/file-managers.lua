e12692779f8d3f46fb7fa186bdc8b9fd8643fcc0
nelson-nsc <nelson.ngai@huboo.com>
2025-03-06 17:03:52 +0000

    group by category

--- .config/nvim/lua/plugins/file-managers.lua
+++ .config/nvim/lua/plugins/file-managers.lua
@@ -53,6 +53,50 @@ return {
     config = function()
       require("yazi").setup(opts)
     end
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      'nvim-telescope/telescope.nvim'
+    },
+    config = function()
+      local harpoon = require('harpoon')
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers").new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        }):find()
+      end
+
+      vim.keymap.set("n", "<leader>fe", function() toggle_telescope(harpoon:list()) end,
+        { desc = "Open harpoon window" })
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+      vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+      vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
+    end
   }
 
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,99 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"python",
-				"css",
-				"javascript",
-				"typescript",
-				"html",
-				"regex",
-				"toml",
-				"yaml",
-				"latex",
-				"markdown",
-				"markdown_inline",
-				"dockerfile",
-			},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "python",
+          "css",
+          "javascript",
+          "typescript",
+          "html",
+          "regex",
+          "toml",
+          "yaml",
+          "latex",
+          "markdown",
+          "markdown_inline",
+          "dockerfile",
+        },
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-			-- List of parsers to ignore installing (or "all")
-			-- ignore_install = { "javascript" },
+        -- List of parsers to ignore installing (or "all")
+        -- ignore_install = { "javascript" },
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = true,
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          -- Defaults
+          enable_close = true,         -- Auto close tags
+          enable_rename = true,        -- Auto rename pairs of tags
+          enable_close_on_slash = true -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        -- per_filetype = {
+        --   ["html"] = {
+        --     enable_close = false
+        --   }
+        -- }
+      })
+    end
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("markview").setup({
+        -- normal mode
+        code_blocks = {
+          style = "minimal",
+          position = nil,
+          min_width = 90,
+          pad_amount = 0,
+          pad_char = " ",
+          -- hl = "CursorLine"
+        },
+      })
+    end,
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup {
+        'css',
+        'javascript',
+        'vue',
+        html = {
+          mode = 'foreground',
+        }
+      }
+    end
+  }
 }
