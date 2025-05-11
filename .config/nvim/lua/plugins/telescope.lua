09be77810ee58a8526267195004074269531a9fd
nelnsc <nelsonn.xyz@gmail.com>
2025-05-11 18:01:09 +0100

    clean up telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,11 +4,9 @@ return {
     branch = "0.1.x",
     dependencies = {
       'nvim-lua/plenary.nvim',
-      "nvim-treesitter/nvim-treesitter",
       "nvim-telescope/telescope-live-grep-args.nvim",
     },
     config = function()
-      require("nvim-treesitter").setup()
       local builtin = require('telescope.builtin')
       local telescope = require("telescope")
       local actions = require "telescope.actions"
@@ -25,7 +23,22 @@ return {
         },
         pickers = {
           find_files = {
-            find_command = { "rg", "-uuu", "--files", "--hidden", "--glob", "!**/.git/*", "-g", "!**/__pycache__/*", "-g", "!**/node_modules/*", "-g", "!**/*_cache/*", "-g", "!**/.venv/**" },
+            find_command = {
+              "rg",
+              "-uuu",
+              "--files",
+              "--hidden",
+              "--glob",
+              "!**/.git/*",
+              "-g",
+              "!**/__pycache__/*",
+              "-g",
+              "!**/node_modules/*",
+              "-g",
+              "!**/*_cache/*",
+              "-g",
+              "!**/.venv/**"
+            },
           },
           live_grep = {
             glob_pattern = { "!**/__pycache__/*" },
@@ -62,12 +75,8 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>fd', builtin.git_commits, {})
-      vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
-      vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
-      vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
       vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-      -- vim.keymap.set('n', '<leader>:', builtin.command_history, {})
-      vim.keymap.set('n', '<leader>:', ":Telescope command_history<CR>!", {})
+      vim.keymap.set('n', '<leader>:', builtin.command_history, {})
       vim.keymap.set('n', '<leader>fy', builtin.registers, {})
       vim.keymap.set("n", "<leader>fw", live_grep_args_shortcuts.grep_word_under_cursor)
     end
