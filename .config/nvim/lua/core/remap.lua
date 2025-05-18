ec7416b4a77a7d5b699d5f1c2747ff0a87073e05
nelnsc <nelsonn.xyz@gmail.com>
2025-05-18 16:31:51 +0100

    clean up

--- .config/nvim/lua/core/remap.lua
+++ .config/nvim/lua/core/remap.lua
@@ -7,14 +7,10 @@ g.mapleader = " "
 vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious)
 
--- local n_keymap = function(lhs, rhs)
---   vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
--- end
--- n_keymap("<Leader>w", ":bp|bd #") -- Close current buffer (disabled, use snacks <leader>bd instead)
+-- vim.api.nvim_set_keymap("n", "<Leader>w", ":bp|bd #", { noremap = true, silent = true }) -- Close current buffer (disabled, use snacks <leader>bd instead)
 
 vim.api.nvim_set_keymap('n', '<tab>', ':if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bnext<CR>', -- Tab to next buffer
   { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<s-tab>',
   ':if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bprevious<CR>',
   { noremap = true, silent = true }) -- Shift-Tab to prvious buffer
-
