c67b35f24d1512d4ee78f02bb3877746ae00e06a
nelson-nsc <nelson.ngai@huboo.com>
2025-12-02 21:45:27 +0000

    clean hapoon

--- .config/nvim/lua/plugins/file-managers.lua
+++ .config/nvim/lua/plugins/file-managers.lua
@@ -4,29 +4,9 @@ return {
     branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      'nvim-telescope/telescope.nvim'
     },
     config = function()
       local harpoon = require('harpoon')
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers").new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        }):find()
-      end
-
-      vim.keymap.set("n", "<leader>fe", function() toggle_telescope(harpoon:list()) end,
-        { desc = "Open harpoon window" })
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
       vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
@@ -78,56 +58,6 @@ return {
     },
     config = function()
       require("yazi").setup(opts)
-    end
-  },
-  {
-    'j-morano/buffer_manager.nvim',
-    config = function()
-      local opts = { noremap = true }
-      local map = vim.keymap.set
-
-      ---- Setup
-      require("buffer_manager").setup({
-        select_menu_item_commands = {
-          v = {
-            key = "<C-v>",
-            command = "vsplit"
-          },
-          h = {
-            key = "<C-h>",
-            command = "split"
-          }
-        },
-        focus_alternate_buffer = false,
-        short_file_names = true,
-        short_term_names = true,
-        loop_nav = false,
-        highlight = 'Normal:BufferManagerBorder',
-        win_extra_options = {
-          winhighlight = 'Normal:BufferManagerNormal',
-        },
-        use_shortcuts = true,
-      })
-
-      ---- Navigate buffers bypassing the menu
-      local bmui = require("buffer_manager.ui")
-      local keys = '1234567890'
-      for i = 1, #keys do
-        local key = keys:sub(i, i)
-        map(
-          'n',
-          string.format('<leader>%s', key),
-          function() bmui.nav_file(i) end,
-          opts
-        )
-      end
-
-      ---- Just the menu
-      map({ 't', 'n' }, '<M-Space>', bmui.toggle_quick_menu, opts)
-
-      ---- Next/Prev
-      -- map('n', '<M-j>', bmui.nav_next, opts)
-      -- map('n', '<M-k>', bmui.nav_prev, opts)
     end
   },
 }
