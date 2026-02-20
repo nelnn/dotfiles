4494e644590769b3e44f5ecd8c410bb3dce92a6a
Nelson Ngai <nsc@Nelson-Huboo.local>
2026-02-20 11:59:48 +0000

    clean up

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -68,7 +68,10 @@ return {
           settings = {
             formatterMode = "typstyle",
             exportPdf = "onType",
-            semanticTokens = "disable"
+            semanticTokens = "disable",
+            formatterProseWrap = true, -- wrap lines in content mode
+            formatterPrintWidth = 80,  -- limit line length to 80 if possible
+            -- formatterIndentSize = 4,   -- indentation width
           }
         },
       }
