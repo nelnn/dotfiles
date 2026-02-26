d4f8d688bed72b6ca851e2bc6190d25d1d5b6e9f
nelnsc <nelsonn.xyz@gmail.com>
2026-02-26 21:38:15 +0000

    use tmux for yazi

--- .config/nvim/lua/core/shortcuts.lua
+++ .config/nvim/lua/core/shortcuts.lua
@@ -1,7 +1,11 @@
 -- Open lazygit in a new tmux window
 vim.keymap.set("n", "<leader>gg", "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazygit <CR><CR>",
-  { desc = "Git Go" })
+  { desc = "lazygit" })
 
 -- Open lazyjj in a new tmux window
 vim.keymap.set("n", "<leader>jj", "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazyjj <CR><CR>",
-  { desc = "Git Go" })
+  { desc = "jj" })
+
+-- Open yazi in a new tmux window
+vim.keymap.set("n", "<leader>y", "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- yazi <CR><CR>",
+  { desc = "yazi" })
